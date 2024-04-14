c4ea68b0d022d8790c2e92b06e4346a7cd386799
rbcoding <rbisht.me@gmail.com>
2024-04-15 09:44:55 +1000

    Delete Genesys Dashboard code.js

--- Genesys Dashboard code.js
+++
@@ -1,7 +0,0 @@
-//Call genesys API and display results
-export default async function () {
-  const url = "https://status.mypurecloud.com/api/v2/components.json";
-  const response = await fetch(url);
-  const result = await response.json();
-  return result.components;
-}
